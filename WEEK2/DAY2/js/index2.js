a7f62be52df5b35f4577bd5e401961768a9b463b
mengqian1117 <2295676706@qq.com>
2017-09-13 17:04:50 +0800

    OK

--- WEEK2/DAY2/js/index2.js
+++ WEEK2/DAY2/js/index2.js
@@ -32,17 +32,24 @@ function bindHTML(data) {
 bindHTML(data);
 
 for (var i=0;i<linkList.length;i++){
-    linkList[i].setAttribute("index",i);
+    //给每一个linkList加一个自定义属性flag,记录当前是升序还是降序
+    linkList[i].setAttribute("flag",-1);
     linkList[i].onclick=function () {
         //this->你点击的元素
-        sortList.call(this)
+        sortList.call(this);
+        changeArrow.call(this);
+        clearOther.call(this);
     }
 }
 function sortList() {
-    //this-->你点击的a标签
-    var index=this.getAttribute("index");
-    //sortAttr  排序的依据
+    //this-->你点击的a标签//sortAttr  排序的依据
     var sortAttr=this.getAttribute("sort-attr");
+    //获取之前的排序状态
+    var flag=this.getAttribute("flag");
+    //改变状态
+    flag*=-1;
+    //将改变后的状态再给当前元素设置上
+    this.setAttribute("flag",flag);
     data.sort(function (a,b) {
         var prev=a[sortAttr];
         var next=b[sortAttr];
@@ -51,7 +58,38 @@ function sortList() {
             prev=prev.replace(/-/g,'');
             next=next.replace(/-/g,'');
         }
-        return prev-next;
+        return (prev-next)*flag;
     });
     bindHTML(data);
+}
+
+//实现箭头的切换
+function changeArrow() {
+    //根据你点击的this,获取出下面的两个i标签
+    var arrows=this.getElementsByTagName("i");
+    var arrowUp=arrows[0], arrowDown=arrows[1];
+    //获取this的flag
+    var flag=this.getAttribute("flag");
+    if(flag>0){
+        arrowUp.className="up bg";
+        arrowDown.className="down";
+    }else {
+        arrowDown.className="down bg";
+        arrowUp.className="up";
+    }
+}
+
+//清除其他A标签的样式(箭头样式)
+function clearOther() {
+    for (var i=0;i<linkList.length;i++){
+        //判断不是你点击的a标签的话,将上下箭头都没有bg这类名
+        //除此之外,我们还要将不是你点击的a标签的排序状态变成最初始的状态flag=-1
+        if(this!==linkList[i]){
+            var arrows=linkList[i].getElementsByTagName("i");
+            arrows[0].className="up";
+            arrows[1].className="down";
+            linkList[i].setAttribute("flag",-1);
+        }
+
+    }
 }
